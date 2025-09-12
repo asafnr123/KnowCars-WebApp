fb1e6aa4c7b4d6c340865e448c59671f11f8f764
Asaf Nahari <asafnr567@gmail.com>
2025-09-12 13:46:39 +0300

    updated the RDS

--- terraform/rds/rds.tf
+++ terraform/rds/rds.tf
@@ -1,8 +1,11 @@
-module "rds_sg" {
-  source = "../security_groups/rds"
-  vpc_id = var.vpc_id
-  vpc_cidr = var.vpc_cidr
+data "aws_security_group" "rds_sg" {
+  filter {
+    name = "tag:Name"
+    values = ["knowcars-rds-sg"]
+   }
 }
+
+
 
 resource "aws_db_subnet_group" "mysql" {
   name       = "knowcars-mysql-subnets"
@@ -20,7 +23,7 @@ resource "aws_db_instance" "mysql" {
   skip_final_snapshot  = true
   publicly_accessible  = false
   db_subnet_group_name = aws_db_subnet_group.mysql.name
-  vpc_security_group_ids = [module.rds_sg.rds_sg_id]
+  vpc_security_group_ids = [data.aws_security_group.rds_sg.id]
 }
 
 
