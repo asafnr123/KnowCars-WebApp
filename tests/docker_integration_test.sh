ae956922d43d9ed59b94c4c0365fe0f7831e832e
asafn <asafnr567@gmail.com>
2026-01-16 01:08:43 +0200

    added check_endpoint function to docker-integration_test

--- tests/docker_integration_test.sh
+++ tests/docker_integration_test.sh
@@ -10,10 +10,8 @@ cleanUp() {
 
 wait_for_flask() {
     local url=$1
-    local max_attempts=15
+    local max_attempts=20
     local attempt=1
-   
-    echo "Waiting for flask to be ready at url: $url"
    
     while [ $attempt -le $max_attempts ]; do
         if curl -sf "$url" > /dev/null 2>&1; then
@@ -23,6 +21,7 @@ wait_for_flask() {
         echo "Attempt $attempt/$max_attempts - Flask is not ready yet..."
         sleep 2
         attempt=$((attempt + 1))
+        
     done
     
     # if flask failed return 1
@@ -32,8 +31,6 @@ wait_for_flask() {
 wait_for_mysql() {
     local max_attempts=20
     local attempt=1
-    
-    echo "Waiting for mysql to be ready"
     
     while [ $attempt -le $max_attempts ]; do
         if docker compose exec -T mysql mysqladmin ping -h mysql -uroot --silent 2>&1; then
@@ -53,7 +50,7 @@ wait_for_mysql() {
 
 check_if_db_exists() {
     
-    if docker compose exec -T mysql bash -c 'mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "USE knowCarsDB;"' 2>/dev/null; then
+    if docker compose exec -T mysql bash -c 'mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "USE knowCarsDB;"' 2>/dev/null; then
         echo "Database exists"
         return 0
     fi
@@ -61,6 +58,31 @@ check_if_db_exists() {
     # if database knowCarsDB does not exists return 1
     return 1
 
+}
+
+
+check_endpoint() {
+    local url=$1
+    local max_attempts=$2
+    local attempt=1
+    
+    
+    while [ $attempt -le $max_attempts ]; do
+    
+        status_code=$(curl -sf -w "%{http_code}" -o /dev/null "$url" 2>&1)
+        if [ "$status_code" = "200" ]; then
+            echo "$url pass, Status code: $status_code"
+            return 0
+        fi
+        
+        echo "Attempt $attempt/$max_attempts - Endpoint is not ready yet"
+        attempt=$((attempt + 1))
+        sleep 2
+        
+    done
+    
+    # if response doesnt return status code 200 return 1
+    return 1
 }
 
 
@@ -76,16 +98,22 @@ main() {
     
     
     if ! wait_for_mysql; then
-       echo "MySQL failed to start"
-       exit 2
+        echo "MySQL failed to start"
+        exit 2
     fi
     
     
     if ! check_if_db_exists; then
-       echo "Database knowCarsDB does not exists"
-       exit 3
+        echo "Database knowCarsDB does not exists"
+        exit 3
     fi
     
+
+    cars_with_images="http://localhost:5000/api/cars?include=image"
+    if ! check_endpoint "$cars_with_images" 10; then    
+        echo "Endpoint at $cars_with_images failed"
+        exit 4
+    fi
     
 }
 
